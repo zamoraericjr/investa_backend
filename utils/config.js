3b3234f99ce27d57b9bd844017c4fbdbc2a6b013
zamoraericjr <zamora.ericjr@gmail.com>
2024-01-09 13:12:21 +0800

    a

--- utils/config.js
+++ utils/config.js
@@ -4,10 +4,12 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 const MONGODB_URI = process.env.MONGODB_URI;
+const JWT_SECRET = process.env.JWT_SECRET;
 
 
 export default {
   PORT,
   MONGODB_URI,
+  JWT_SECRET,
  
 };
