00615f88c5921bcd3ec07f0e1a7ed9e0d6e3f5a3
zamoraericjr <zamora.ericjr@gmail.com>
2024-01-19 16:52:09 +0800

    ok

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ morgan.token("body", function (req, res) {
   return JSON.stringify(req.body);
 });
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(express.static("dist"));
 app.use(morgan(":method :url :status :body"));
